2b94a0b25b1ab2d76df6a04e59f1351f17357abf
ntrx <kraenash@gmail.com>
2023-11-30 12:00:10 +0100

    add: init files

---
+++ src/main/java/com/sdv/npt/npt_book_rental/NptBookRentalApplication.java
@@ -0,0 +1,13 @@
+package com.sdv.npt.npt_book_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NptBookRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NptBookRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdv/npt/npt_book_rental/NptBookRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdv.npt.npt_book_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NptBookRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
